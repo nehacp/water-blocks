f168aeac49be0f735c224187b70399ae70cd4b59
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-01-26 14:22:22 -0800

    Add environment for linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-    "extends": "./node_modules/eslint-config-airbnb-base/index.js"
+    "extends": "./node_modules/eslint-config-airbnb-base/index.js",
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true,
+        "mocha": true
+    }
 };
