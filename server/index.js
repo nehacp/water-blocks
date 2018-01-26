195d486621baf96ab271b0654e03640a062a40f0
Neha Chaudhary <neha.chaudhary@hotmail.com>
2018-01-26 13:54:41 -0800

    Set up server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,14 @@
 const express = require ('express');
-const app = app();
+const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3333;
 const waterBlocks = require('./water-blocks');
 
 app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+})
 
 app.post('/maxwater', (req, res) => {
   /// this is the request handler for returning the answer;
